bc5a381b26e261c542f2666b1cd95ff56218c938
erascu <e.rashku@gmail.com>
2025-01-13 12:25:44 +0000

    add custom colours to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,11 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        fitGrey: "#F6F6F6",
+        fitViolet: "#857CED",
+        fitBlue: "#091B35",
+        fitGreen: "#D3F076",
+        fitRed: "#FD6131",
       },
     },
   },
