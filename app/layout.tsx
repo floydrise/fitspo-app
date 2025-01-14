df59b553564b9630bfd21b9f5b05d609681041f5
tylerkade <tylerkade@live.co.uk>
2025-01-14 17:16:25 +0000

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/app/ui/Header";
-import Footer from "@/app/ui/footer";
-
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Header from '@/app/ui/Header';
+import Footer from '@/app/ui/footer';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "FitSpo",
-  description: "Exercise tracking made easy",
+  title: 'FitSpo',
+  description: 'Exercise tracking made easy',
 };
 
 export default function RootLayout({
@@ -26,13 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Header/>
-       <main>{children}</main>
-        {children}
+        <Header />
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
