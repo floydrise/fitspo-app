53aca463643f43cc42cae4f5ee9004ed371dc6e1
tylerkade <tylerkade@live.co.uk>
2025-01-14 10:44:19 +0000

    Finished fetchWorkoutHistory endpoint, and progress on postWorkoutHistory endpoint

--- app/lib/endpoints.ts
+++ app/lib/endpoints.ts
@@ -1,4 +1,5 @@
 import { sql } from '@vercel/postgres';
+import { Workout_history } from './definitions';
 
 export async function fetchUsers() {
   try {
@@ -31,18 +32,87 @@ export async function fetchWorkouts() {
   }
 }
 
-// export async function fetchExerciseById(exercise_id: string) {
-//   try {
-//     const response = await fetch(
-//       `https://exercisedb-api.vercel.app/api/v1/exercises/${exercise_id}}`
-//     );
-//     if (!response.ok) {
-//       throw new Error(`Error fetching exercise with ID: ${exercise_id}`);
-//     }
-//     const exercise = await response.json();
-//     return exercise;
-//   } catch (error) {
-//     console.error(`Error fetching exercise with ID: ${exercise_id}`, error);
-//     throw new Error(`Error fetching exercise with ID: ${exercise_id}`);
-//   }
-// }
+export async function fetchWorkoutById(workout_id: number) {
+  try {
+    const data = await sql`SELECT * FROM workouts
+    WHERE workout_id = ${workout_id}`;
+    return data.rows[0];
+  } catch (error) {
+    console.error('error fetching workout', error);
+    throw new Error('error fetching workout');
+  }
+}
+
+export async function fetchWorkoutHistoryByUserId(user_id: number) {
+  try {
+    const data = await sql`SELECT * FROM workout_history
+    WHERE user_id = ${user_id}`;
+    return data.rows;
+  } catch (error) {
+    console.error('error fetching workout history ', error);
+    throw new Error('error fetching workout history');
+  }
+}
+/*
+async function postWorkoutHistory(values: Workout_history) {
+  const {
+    user_id,
+    workout_id,
+    date,
+    duration,
+    exercise_list,
+  } = values;
+
+  console.log('Saving workout history to database:', {
+    user_id,
+    workout_id,
+    date,
+    duration,
+    exercise_list,
+  });
+}
+
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    // Process a POST request
+    console.log(req.body)
+      try {
+        const { user_id, workout_id, date, duration, exercise_list } = req.body
+        if (!user_id || !workout_id || !date || !duration || !Array.isArray(exercise_list)) {
+          return res.status(400).json({error: "invalid request"})
+        }
+
+        const workoutHistoryId = await postWorkoutHistory({
+          user_id,
+          workout_id,
+          date,
+          duration,
+          exercise_list,
+        });
+
+
+      
+    }
+  }
+}
+*/
+
+export async function fetchExerciseById() {
+  try {
+    const response = await fetch(
+      `https://exercisedb-api.vercel.app/api/v1/exercises/I4hDWkc`
+    );
+    if (!response.ok) {
+      throw new Error(`Error fetching exercise with ID: I4hDWkc`);
+    }
+    const exercise = await response.json();
+    console.log(exercise, 'exercise');
+    return exercise;
+  } catch (error) {
+    console.error(`Error fetching exercise with ID: I4hDWkc`, error);
+    throw new Error(`Error fetching exercise with ID: I4hDWkc`);
+  }
+}
---
+++ app/tests/postWH/page.tsx
@@ -0,0 +1,12 @@
+// import { handler } from '@/app/lib/endpoints';
+import React from 'react';
+
+async function page() {
+  return (
+    <div>
+      <h1>workout history</h1>
+    </div>
+  );
+}
+
+export default page;
