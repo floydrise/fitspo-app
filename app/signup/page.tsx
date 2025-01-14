65d955da73880d90e2dad69a226150fe4d24b67a
Stefan <floydrise@gmail.com>
2025-01-14 16:01:43 +0200

    Implement form for regestering new users

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import SignupForm from '@/app/ui/SignupForm';
+import {HashtagIcon} from '@heroicons/react/24/outline';
 
 const Page = () => {
   return (
-    <div>
-      Sign up page yeehaaw
-    </div>
-  );
-};
+    <div className={"flex flex-col justify-center items-center w-full p-10 gap-2"}>
+      <span className={"flex gap-1"}><HashtagIcon className={"w-5"} /><h1 className={"text-2xl"}>Sign up</h1></span>
+      <div
+        className={'flex w-full md:w-2/5 justify-center rounded-md border bg-indigo-50 p-4'}
+      >
+        <SignupForm />
+      </div>
+      </div>
+      );
+      };
 
-export default Page;
+      export default Page;
