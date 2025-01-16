9fa13b31147a4c885b4d29752ec0203a2a9b89b8
erascu <e.rashku@gmail.com>
2025-01-16 10:03:34 +0000

    add functionality to filter

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,8 +1,21 @@
 'use client';
-import ExerciseCard from '../ui/ExerciseCard';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import ExerciseCard from '../ui/ExerciseCard';
+import ExerciseInfo from '../ui/ExerciseInfo';
 import Form from 'next/form';
+import { useDebouncedCallback } from 'use-debounce';
+// import Link from 'next/link';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+
 import { useState, useEffect } from 'react';
 
 interface Exercise {
@@ -13,6 +26,15 @@ interface Exercise {
 const Page = () => {
   const [exercises, setExercises] = useState<Exercise[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [bodyFilter, setBodyFilter] = useState<string>('all');
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // const filter = searchParams.get('filterBy');
+  // console.log(filter);
+  // const pageNumber: number = parseInt(searchParams.get('page') || '0') * 12;
 
   const filterNames: string[] = [
     'All',
@@ -28,20 +50,28 @@ const Page = () => {
     'Cardio',
   ];
 
+  const handleSearch = useDebouncedCallback((event: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (event) {
+      params.set('search', event);
+    } else {
+      params.delete('search');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   useEffect(() => {
     setIsLoading(true);
     async function fetchExercises() {
       const res = await fetch(
-        'https://exercisedb-api.vercel.app/api/v1/exercises'
+        `https://exercisedb-api.vercel.app/api/v1/${bodyFilter === 'all' ? '' : 'bodyparts/' + bodyFilter + '/'}exercises?offset=0&limit=12`
       );
-      const {
-        data: { exercises },
-      } = await res.json();
-      setExercises(exercises);
+      const { data } = await res.json();
+      setExercises(data.exercises);
       setIsLoading(false);
     }
     fetchExercises();
-  }, []);
+  }, [bodyFilter]);
 
   return (
     <section className='container mx-auto px-[15px] pb-[50px] pt-[26px]'>
@@ -49,25 +79,34 @@ const Page = () => {
         <ul className='flex overflow-y-auto'>
           {filterNames.map((name) => (
             <li key={name} className='mr-[10px] last:mr-0'>
-              <Button variant='filterBtn' size='fit'>
-                <Link href={'/exercises'}>{name}</Link>
+              <Button
+                variant='filterBtn'
+                size='fit'
+                onClick={() => setBodyFilter(name.toLowerCase())}
+              >
+                {name}
+                {/* <Link href={'/exercises'}>{name}</Link> */}
               </Button>
             </li>
           ))}
         </ul>
       </div>
       <div className='mt-[30px] flex justify-center'>
-        <Form action='/search'>
+        <Form action='/exercises'>
           <input
-            className='mr-[16px] h-[40px] w-[200px] rounded-[15px] border border-[#89A1A9] px-[15px] py-[12px] text-sm text-fitBlue focus:outline-none sm:w-[300px] md:w-[400px] lg:w-[560px]'
+            className='h-[40px] w-[200px] rounded-[15px] border border-[#89A1A9] px-[15px] py-[12px] text-sm text-fitBlue focus:outline-none sm:w-[300px] md:w-[400px] lg:w-[560px]'
             name='query'
+            placeholder='search exercises'
+            defaultValue={searchParams.get('search')?.toString()}
+            onChange={(e) => handleSearch(e.target.value)}
           />
-          <Button type='submit' variant='searchBtn' size='searchSize'>
+          {/* <Button type='submit' variant='searchBtn' size='searchSize'>
             Search
-          </Button>
+          </Button> */}
         </Form>
       </div>
       <h2 className='text-[32px] font-semibold text-fitBlue'>Exercises</h2>
+      {/* <ExerciseInfo /> */}
       <div>
         <ul className='justify-content-between mt-[26px] grid justify-items-center gap-8 md:grid-cols-2 xl:grid-cols-3'>
           {isLoading
@@ -79,6 +118,22 @@ const Page = () => {
               ))}
         </ul>
       </div>
+      <Pagination className='mt-[30px]'>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href='#' />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href='#'>1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href='#' />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </section>
   );
 };
