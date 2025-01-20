7289c0c140a6e3a35895e89568c31d1070afe46b
tylerkade <tylerkade@live.co.uk>
2025-01-20 17:01:30 +0000

    Styling mostly done, some minor functionality

--- app/tracker/[workout_name]/page.tsx
+++ app/tracker/[workout_name]/page.tsx
@@ -3,6 +3,7 @@ import { fetchExerciseById, fetchWorkoutByName } from '@/app/lib/endpoints';
 import { notFound } from 'next/navigation';
 import { Workout } from '@/app/lib/definitions';
 import TrackerCard from '@/app/ui/TrackerCard';
+import { Button } from '@/components/ui/button';
 
 export default async function Page(props: {
   params: Promise<{ workout_name: string }>;
@@ -23,13 +24,26 @@ export default async function Page(props: {
   const exerciseNames: string[] = await Promise.all(exercises);
 
   return (
-    <div>
-      <p>{workout.workout_name}</p>
+    <section>
+      <div className='flex items-center justify-center'>
+        {/* Timer will go here, and workout name will be on left-hand side of it */}
+        <h1 className='pt-1 text-2xl font-bold'>
+          {workout.workout_name} workout
+        </h1>
+      </div>
       {exerciseNames.map((exerciseName) => (
-        <ul key={exerciseName}>
+        <ul key={exerciseName} className='flex items-center justify-center p-1'>
           <TrackerCard exerciseName={exerciseName} />
         </ul>
       ))}
-    </div>
+      <div className='flex items-center justify-center gap-20'>
+        <button className='rounded-[15px] bg-fitGreen px-[33px] py-[10px] text-[18px] font-bold transition ease-in-out hover:bg-fitBlue hover:text-fitGreen'>
+          Finish
+        </button>
+        <button className='rounded-[15px] bg-fitBlue px-[33px] py-[10px] text-[18px] font-bold text-fitGreen transition ease-in-out hover:bg-fitRed hover:text-fitBlue'>
+          Cancel
+        </button>
+      </div>
+    </section>
   );
 }
