9323f9292e3d38fa67e2a9a05653d348a688c125
erascu <e.rashku@gmail.com>
2025-01-21 16:31:55 +0000

    refactor a lil footer styling

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -3,37 +3,37 @@ import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <footer className='p-4 text-center text-gray-500'>
+    <footer className='pb-12 pt-4 text-center text-gray-500'>
       {/* Footer Content */}
       <div className='container mx-auto flex flex-col items-center justify-between space-y-4 md:flex-row md:space-y-0'>
         {/* Left Side, Home, About, and Contact */}
-        <nav className='flex space-x-6'>
+        <nav className='flex justify-between'>
           <Link
             href='/'
-            className='rounded-lg px-3 py-1 hover:bg-gray-200 hover:text-gray-800'
+            className='rounded-lg px-3 py-1 hover:bg-gray-200 hover:text-gray-800 sm:mr-3'
           >
             Home
           </Link>
           <Link
             href='/about'
-            className='rounded-lg px-3 py-1 hover:bg-gray-200 hover:text-gray-800'
+            className='rounded-lg px-3 py-1 hover:bg-gray-200 hover:text-gray-800 sm:mr-3'
           >
             About
           </Link>
           <Link
             href='/contact'
-            className='rounded-lg px-3 py-1 hover:bg-gray-200 hover:text-gray-800'
+            className='rounded-lg px-3 py-1 hover:bg-gray-200 hover:text-gray-800 sm:mr-3'
           >
             Contact
           </Link>
-          <a
+          <Link
             href='https://github.com/floydrise/fitspo-app'
             target='_blank'
             rel='noopener noreferrer'
             className='rounded-lg px-3 py-1 hover:bg-gray-200 hover:text-gray-800'
           >
             Github
-          </a>
+          </Link>
         </nav>
 
         {/* Center FitSpo Logo */}
