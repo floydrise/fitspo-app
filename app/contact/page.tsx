36c091ffc854fbdf968888db9bb305f2fdadef83
erascu <e.rashku@gmail.com>
2025-01-22 16:54:51 +0000

    add contact component and add info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,43 +1,68 @@
 import React from 'react';
+import AboutCard from '../ui/AboutCard';
 
 export default function Contact() {
-  const team = [
-    'Ayoub Chemmach',
-    'Eugeniu Rascu',
-    'Stefan Petrov',
-    'Tyler Mulligan',
-    'Yahya Islam',
-    'Yousuf',
-  ];
-  const githubUsername = [
-    'ayoub1400',
-    'erascu',
-    'floydrise',
-    'tylerkade',
-    'yahya436',
-    'Youxuf',
+  const ourTeam: Array<{
+    id: number;
+    name: string;
+    gitHub: string;
+    mail: string;
+    linkedin: string;
+  }> = [
+    {
+      id: 1,
+      name: 'Ayoub Chemmach',
+      gitHub: 'ayoub1400',
+      mail: 'ayoubc04@hotmail.com',
+      linkedin: '',
+    },
+    {
+      id: 2,
+      name: 'Eugeniu Rascu',
+      gitHub: 'erascu',
+      mail: 'e.rashku@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/erascu/',
+    },
+    {
+      id: 3,
+      name: 'Stefan Petrov',
+      gitHub: 'floydrise',
+      mail: 'floydrise@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/stefan-petrov-6404401bb/',
+    },
+    {
+      id: 4,
+      name: 'Tyler Mulligan',
+      gitHub: 'tylerkade',
+      mail: 'tykdm@outlook.com',
+      linkedin: 'https://www.linkedin.com/in/tyler-kd-mulligan/',
+    },
+    {
+      id: 5,
+      name: 'Yahya Islam',
+      gitHub: 'yahya436',
+      mail: 'yahyaislam436@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/yahya-islam-486560333/',
+    },
+    {
+      id: 6,
+      name: 'Yousuf Mohammed',
+      gitHub: 'Youxuf',
+      mail: 'yousuf437@gmail.com',
+      linkedin: '',
+    },
   ];
 
   return (
-    <div className='container mx-auto mt-2 rounded-lg bg-fitViolet p-6 text-white'>
-      <h1 className='flex justify-center text-2xl font-bold'>Contact Us</h1>
-      <h2 className='flex justify-center'>Our Team</h2>
-      <div className='m-[1em] flex flex-row flex-wrap justify-center gap-[1em]'>
-        {team.map((name, index) => (
-          <a
-            key={index}
-            href={`https://github.com/${githubUsername[index]}`}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='shadow:lg mx-auto block w-[150px] max-w-sm rounded-lg border border-fitGreen bg-fitGreen p-4 text-fitBlue transition-shadow hover:border-fitBlue hover:bg-fitBlue hover:text-fitGreen hover:shadow-2xl'
-          >
-            <div className='text-center'>
-              <h2 className='mb-2 text-xl font-bold'>{name}</h2>
-              <p className='text-gray-600'></p>
-            </div>
-          </a>
+    <div className='container mx-auto px-[15px] pb-[50px] pt-[26px]'>
+      <h1 className='text-[32px] font-semibold text-fitBlue'>Contact Us</h1>
+      <ul className='justify-content-between mt-[26px] grid justify-items-center gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {ourTeam.map((info) => (
+          <li key={info.id}>
+            <AboutCard info={info} />
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
