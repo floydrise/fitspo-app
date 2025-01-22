07c2005a9635864c7e9771d7b488f32ebc29a5d0
Stefan <floydrise@gmail.com>
2025-01-22 15:43:28 +0200

    Display history in descending order - latest workout first in the list

--- app/ui/timer.tsx
+++ app/ui/timer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { formatTime } from '@/lib/utils';
+import { formatTime } from "@/lib/utils";
 
 const Timer = ({
   setTimeAction,
