7e17d930555a04b6f6d44c853491a58dae1d56f1
erascu <e.rashku@gmail.com>
2025-01-23 17:00:17 +0000

    change styling

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,23 +4,21 @@ import { HashtagIcon } from '@heroicons/react/24/outline';
 
 const Page = () => {
   return (
-    <div
+    <section
       className={
-        'flex h-screen w-screen flex-col items-center justify-center gap-2 p-10'
+        'container mx-auto mt-10 flex max-w-[400px] flex-col items-center justify-center rounded-[15px] bg-white shadow-md'
       }
     >
-      <span className={'flex gap-1'}>
-        <HashtagIcon className={'w-5'} />
-        <h1 className={'text-2xl'}>Sign up</h1>
-      </span>
-      <div
-        className={
-          'flex w-full justify-center rounded-md shadow-md bg-indigo-200 p-4 md:w-1/3'
-        }
-      >
+      <div className='h-28 w-full rounded-t-[15px] bg-fitViolet p-7 text-center'>
+        <h1 className={'text-2xl font-semibold text-white'}>
+          Create an account
+        </h1>
+        <p className='text-white'>Sign up now and unlock Workout access!</p>
+      </div>
+      <div className={'flex w-full justify-center p-4 pb-10 md:w-1/3'}>
         <SignupForm />
       </div>
-    </div>
+    </section>
   );
 };
 
