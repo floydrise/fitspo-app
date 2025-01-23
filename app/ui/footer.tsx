a2ddc6abc935bac619f70cd1923e977f9047251b
erascu <e.rashku@gmail.com>
2025-01-23 10:34:29 +0000

    remove margin top from logo

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <footer className='pb-12 pt-4 text-center text-gray-500'>
+    <footer className='px-[15px] pb-12 pt-4 text-center text-gray-500'>
       <div className='container mx-auto flex flex-col items-center justify-between space-y-4 md:flex-row md:space-y-0'>
         <nav className='flex justify-between'>
           <Link
@@ -40,7 +40,7 @@ export default function Footer() {
               width={1440}
               height={531}
               alt='Monochrome FitSpo Logo'
-              className='mx-auto mt-4 h-10 w-auto'
+              className='mx-auto h-10 w-auto'
             />
           </Link>
         </div>
